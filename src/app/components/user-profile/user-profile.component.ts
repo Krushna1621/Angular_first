cc138e4f23d881cd5bc03bee0210ab2223f753c2
Krushnakant Shinde <shindekrushna825@gmail.com>
2024-04-22 16:38:57 +0530

    how to use directive (ngModel),ngFor,ngIf

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, isSignal } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+   name="ram";
+   status="single";
+   salary=8829839000;
+   isBtnDisabled=false;
+   inputValue="test";
 
+users=[
+  {
+    name:"Ram",
+    isSingle:true,
+    salary:9
+  },
+  {
+    name:"Shyam",
+    isSingle:false,
+    salary:8829839000
+  },
+  {
+    name:"Ganesh",
+    isSingle:false,
+    salary:893738000
+  }
+]
+   
+  //  onChange(e:Event){
+  //   const value =(e.target as HTMLInputElement).value;
+  //  this.inputValue=value
+  //  }
 }
