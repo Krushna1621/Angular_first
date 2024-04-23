5fef07b6c3902fa0b4a81185e39281b491c819d1
Krushnakant Shinde <shindekrushna825@gmail.com>
2024-04-23 12:53:38 +0530

    make reusable component by using input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserProfileComponent],
+  imports: [RouterOutlet,UserProfileComponent,CommonModule],
   templateUrl: './app.component.html',
 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-ng-app';
+  users=[
+  {
+    name:"Ram",
+    isSingle:true,
+    salary:9
+  },
+  {
+    name:"Shyam",
+    isSingle:false,
+    salary:8
+  },
+  {
+    name:"Ganesh",
+    isSingle:true,
+    salary:8
+  }
+]
 }
